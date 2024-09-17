5a801e4f036de39ef9fa3f7283506df82f370288
monshanlin <monshanlin@gmail.com>
2024-09-16 19:10:17 -0700

    Complete week2 in class activity

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
+  const appName = "Mon-Shan's app!"; 
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
       <StatusBar style="auto" />
+      <Header name={appName}/>
+      <Input />
     </View>
   );
 }
@@ -13,8 +17,4 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   
