a346dc68e6b955115abbe3c50db35cb33e8776cc
monshanlin <monshanlin@gmail.com>
2024-09-17 08:07:01 -0700

    completed lab1

---
+++ components/Input.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import { useState } from 'react'
+import { TextInput, Text } from 'react-native';
+
+export default function Inputt({ focus }) {
+    const [text, setText] = useState("");
+    const [count, setCount] = useState(0);
+    const [isFocused, setIsFocused] = useState(focus);
+    return (
+        <>
+        <TextInput
+            placeholder="Type something here"
+            autoCorrect={true}
+            keyboardType="default"
+            value={text}
+            style={{ borderBottomColor: "purple", borderBottomWidth: 2 }}
+            onChangeText={(changedText) => {
+                setText(changedText);
+                setCount(changedText.length);
+            }}
+            autoFocus={focus}
+            onFocus={() => setIsFocused(true)}
+            onSubmitEditing={() => setIsFocused(false)}
+        />
+        {isFocused ? (
+            count > 0 ? <Text>Number of Characters: {count}</Text> : null
+        ) : (
+            count < 3 ? <Text>Please type more than 3 characters</Text> : <Text>Thank you</Text>
+        )}
+        </>
+    )
+}
