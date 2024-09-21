ea76e59ac32b5dd4a6d21ca98b32fec2a06f25f1
monshanlin <monshanlin@gmail.com>
2024-09-21 13:20:21 -0700

    Declare a const variable named appName

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
+  const appName = "Mon-Shan's app!"; 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
     </View>
   );
