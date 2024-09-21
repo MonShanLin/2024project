1857f90508882358f9f1fa4b6e1b6f7136d249fa
monshanlin <monshanlin@gmail.com>
2024-09-21 14:28:31 -0700

    Add autoFocus to control whether the TextInput should have focus when the component renders.

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 
 export default function App() {
   const appName = "Mon-Shan's app!"; 
   const inputFocus = true;
+  
   return (
     <View style={styles.container}>
+      <Header name={appName} />
       <StatusBar style="auto" />
-      <Header name={appName}/>
       <Input focus={inputFocus}/>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- components/Input.js
+++ components/Input.js
@@ -1,32 +1,20 @@
 import React from 'react'
 import { useState } from 'react'
-import { TextInput, Text } from 'react-native';
-
-export default function Inputt({ focus }) {
+import { TextInput } from 'react-native';
+export default function Input() {
     const [text, setText] = useState("");
-    const [count, setCount] = useState(0);
-    const [isFocused, setIsFocused] = useState(focus);
+    
     return (
-        <>
         <TextInput
+            value={text}
             placeholder="Type something here"
+            autoFocus={true} 
             autoCorrect={true}
             keyboardType="default"
-            value={text}
             style={{ borderBottomColor: "purple", borderBottomWidth: 2 }}
             onChangeText={(changedText) => {
-                setText(changedText);
-                setCount(changedText.length);
+                setText(changedText);    
             }}
-            autoFocus={focus}
-            onFocus={() => setIsFocused(true)}
-            onSubmitEditing={() => setIsFocused(false)}
         />
-        {isFocused ? (
-            count > 0 ? <Text>Number of Characters: {count}</Text> : null
-        ) : (
-            count < 3 ? <Text>Please type more than 3 characters</Text> : <Text>Thank you</Text>
-        )}
-        </>
     )
 }
