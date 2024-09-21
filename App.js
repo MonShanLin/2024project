7f7940c365e63fa85d29f4a3f47580e07dd503b1
monshanlin <monshanlin@gmail.com>
2024-09-21 13:08:18 -0700

    Revert back to inital commit

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import Input from './components/Input';
-
 export default function App() {
-  const appName = "Mon-Shan's app!"; 
   return (
     <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Header name={appName}/>
-      <Input />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
