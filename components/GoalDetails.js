bed1721d97a70cf217dfd07da02a69635e480e87
monshanlin <monshanlin@gmail.com>
2024-10-01 14:27:37 -0700

    Add “More Details” Button to GoalDetails page

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-export default function GoalDetails ({ route }) {
+export default function GoalDetails ({ route, navigation }) {
 
-  const { goal } = route.params;
+  const { goal, moreDetails } = route.params;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Goal Details</Text>
-      <Text style={styles.goalText}>Goal: {goal.text}</Text> 
-      <Text style={styles.goalId}>ID: {goal.id}</Text>
+        {!moreDetails ? (
+            <>
+                <Text style={styles.title}>Goal Details</Text>
+                <Text style={styles.goalText}>You are seeing the details of the goal with text: {goal.text} and id: {goal.id}</Text> 
+                
+
+                <Button
+                title="More details"
+                onPress={() => navigation.push('Details', { goal,moreDetails: true  })} 
+                />
+            </>
+        ) : (
+            <>
+                <Text style={styles.moreDetailsText}>More Details</Text> 
+            
+                <Button
+                    title="More details"
+                    onPress={() => navigation.push('Details', { goal,moreDetails: true  })} 
+                />
+            </>
+        )}
     </View>
   );
 };
@@ -18,11 +36,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
-    padding: 16,
+    padding: 20,
   },
- 
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
   goalText: {
     fontSize: 16,
     color: 'black',
   },
+  goalId: {
+    fontSize: 14,
+    color: 'gray',
+    marginTop: 10,
+  },
+  moreDetailsText: {
+    fontSize: 18,
+    color: 'black',
+    },
 });
