e769b0199af27e0bf817810e3cccd54b63397ce7
monshanlin <monshanlin@gmail.com>
2024-10-01 14:07:05 -0700

    Pass a JavaScript object to options prop on the Stack.Screen component

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'; // Import NavigationContainer
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Import createNativeStackNavigator
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
 import GoalDetails from './components/GoalDetails';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +11,15 @@ export default function App() {
   return (
     <NavigationContainer> 
       <Stack.Navigator>
-        
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="Home" 
+        component={Home} 
+        options={{
+          title: "All Goals", 
+          headerStyle: { backgroundColor: 'purple' }, 
+          headerTintColor: 'white',
+          }}
+        />
         <Stack.Screen name="Details" component={GoalDetails} />
 
       </Stack.Navigator>
