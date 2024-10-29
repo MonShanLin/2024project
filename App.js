244dbf78dbbd99ff85fa93f2ba999608ee2c5cdb
monshanlin <monshanlin@gmail.com>
2024-10-29 13:57:52 -0700

    Update app.js to nevigate to login and signup pages

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { StyleSheet, SafeAreaView, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
 import GoalDetails from './components/GoalDetails';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,12 +12,32 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator
+        initialRouteName="Login" // Set Login as the initial screen
         screenOptions={{
           headerStyle: { backgroundColor: 'purple' },
           headerTintColor: 'white',
           headerTitleStyle: { fontWeight: 'bold' },
         }}
       >
+        {/* Login Screen */}
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: 'Login',
+          }}
+        />
+
+        {/* Signup Screen */}
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            title: 'Signup',
+          }}
+        />
+          
+          {/* Home Screen */}
         <Stack.Screen
           name="Home"
           component={Home}
@@ -23,6 +45,8 @@ export default function App() {
             title: 'All Goals',
           }}
         />
+          
+          {/* Details Screen */}
         <Stack.Screen
           name="Details"
           component={GoalDetails}
