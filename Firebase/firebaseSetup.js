255a952049e4e89c55a89d168db17cb81c80590f
monshanlin <monshanlin@gmail.com>
2024-11-05 13:49:04 -0800

    Add security rule

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -4,6 +4,8 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
 // Import initializeAuth and getReactNativePersistence
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth'; 
+import { getStorage } from "firebase/storage";
+
 
 //Your web app's Firebase configuration. 
 //Copy this object from Firebase console
@@ -11,7 +13,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDGYTsGTABpu-Z6uV9ncxqJ-8tKG0F3ul0",
     authDomain: "cs5520-2cead.firebaseapp.com",
     projectId: "cs5520-2cead",
-    storageBucket: "cs5520-2cead.appspot.com",
+    storageBucket: "cs5520-2cead.firebasestorage.app",
     messagingSenderId: "801980424038",
     appId: "1:801980424038:web:1af2938570b5b76dc032a7",
     measurementId: "G-671SFEG3TZ"
@@ -19,6 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
 
 // Initialize Firestore
 export const database = getFirestore(app);
